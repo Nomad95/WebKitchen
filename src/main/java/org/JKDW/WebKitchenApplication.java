419abc0ca1cffe030f6a6fa75082f8f9f9175cfb
Konrad Depta <konradd93@gmail.com>
2016-12-16 20:13:06 +0100

    Inicjalizacja

---
+++ src/main/java/org/JKDW/WebKitchenApplication.java
@@ -0,0 +1,12 @@
+package org.JKDW;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebKitchenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebKitchenApplication.class, args);
+	}
+}
