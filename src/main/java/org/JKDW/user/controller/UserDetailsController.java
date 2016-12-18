5802f7165a90c75960c0de60fcbc355b9021f11a
Konrad Depta <konradd93@gmail.com>
2016-12-18 23:50:54 +0100

    name->value

--- src/main/java/org/JKDW/user/controller/UserDetailsController.java
+++ src/main/java/org/JKDW/user/controller/UserDetailsController.java
@@ -22,13 +22,13 @@ public class UserDetailsController {
 	@Autowired
 	private UserDetailsService userDetailsService;
 	
-	@RequestMapping( name="/all",method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	@RequestMapping( value="/all",method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<List<UserDetails>> getUserDetails(){
 		List<UserDetails> userDetails = userDetailsService.getAllUserDetails();
 		return new ResponseEntity<>(userDetails,HttpStatus.OK);
 	}
 	
-/*	@RequestMapping(name="/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+/*	@RequestMapping(value="/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<UserDetails> getUserDetailsByUser(@RequestBody UserAccount userAccount){
 		UserDetails userDetails = userDetailsService.getUserDetailsByUser(userAccount);
 		return new ResponseEntity(userDetails,HttpStatus.OK);
@@ -40,7 +40,7 @@ public class UserDetailsController {
 	 * @param userAccount - to know to which user account we make new datails
 	 * @return created details
 	 */
-	@RequestMapping(name="/create",method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	@RequestMapping(value="/create",method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<UserDetails> createUserDetails(@RequestBody UserDetails userDetails,@RequestBody UserAccount userAccount){
 		UserDetails createdUserDetails = userDetailsService.createUserDetails(userDetails, userAccount.getId());
 		return new ResponseEntity(createdUserDetails,HttpStatus.CREATED);
@@ -52,7 +52,7 @@ public class UserDetailsController {
 	 * @param userDetails to update
 	 * @return updated user details
 	 */
-	@RequestMapping(name="/{id}",method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	@RequestMapping(value="/{id}",method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<UserDetails> updateUserDetails(@RequestBody UserDetails userDetails){
 		UserDetails updatedUserDetails = userDetailsService.updateUserDetails(userDetails);
 		return new ResponseEntity(updatedUserDetails,HttpStatus.OK);
@@ -62,7 +62,7 @@ public class UserDetailsController {
 	 * 
 	 * @param id - id of deleting detail
 	 */
-	@RequestMapping(name="/{id}",method = RequestMethod.DELETE)
+	@RequestMapping(value="/{id}",method = RequestMethod.DELETE)
 	public ResponseEntity deleteUserDetails(@PathVariable("id") Long id){
 		userDetailsService.deleteUserDetails(id);
 		return new ResponseEntity(HttpStatus.OK);
