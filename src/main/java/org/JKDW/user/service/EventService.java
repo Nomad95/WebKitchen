f917de90c0566f65f2e244ac9b0526dc90a276b7
igor-kopec <igokop99@gmail.com>
2017-03-01 21:31:55 +0100

    i crudzik

---
+++ src/main/java/org/JKDW/user/service/EventService.java
@@ -0,0 +1,19 @@
+package org.JKDW.user.service;
+
+import org.JKDW.user.model.Event;
+
+import javax.persistence.NoResultException;
+import java.util.List;
+
+public interface EventService {
+
+    List<Event> getAllEvents();
+
+    Event getEventById(Long id);
+
+    Event createEvent(Event event);
+
+    Event updateEvent(Event event) throws NoResultException;
+
+    void deleteEvent(Long id) throws NoResultException;
+}
