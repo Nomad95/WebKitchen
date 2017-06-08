0822d76a3d5c70ea435f0a1188081c5083074433
intelcan <intelcan.idea@gmail.com>
2017-06-08 12:05:52 +0200

    DB message

--- src/main/java/org/JKDW/user/model/Message.java
+++ src/main/java/org/JKDW/user/model/Message.java
@@ -1,7 +1,88 @@
 package org.JKDW.user.model;
 
-/**
- * Created by intelcan on 07.06.17.
- */
+import org.hibernate.annotations.Type;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
 public class Message {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "sender")
+    private UserAccount sender;
+
+    private String title;
+
+    @OneToOne
+    @JoinColumn(name = "recipient")
+    private UserAccount recipient;
+
+    @Column(name = "date_of_send")
+    private LocalDate dateOfSend;
+
+    @Lob
+    @Column(name = "message_contents")
+    @Type(type = "org.hibernate.type.TextType")
+    private String messageContents;
+
+
+    @Column(name = "was_read")
+    private Boolean wasRead;
+
+    public Long getId() {
+        return id;
+    }
+
+    public UserAccount getSender() {
+        return sender;
+    }
+
+    public void setSender(UserAccount sender) {
+        this.sender = sender;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public UserAccount getRecipient() {
+        return recipient;
+    }
+
+    public void setRecipient(UserAccount recipient) {
+        this.recipient = recipient;
+    }
+
+    public LocalDate getDateOfSend() {
+        return dateOfSend;
+    }
+
+    public void setDateOfSend(LocalDate dateOfSend) {
+        this.dateOfSend = dateOfSend;
+    }
+
+    public String getMessageContents() {
+        return messageContents;
+    }
+
+    public void setMessageContents(String messageContents) {
+        this.messageContents = messageContents;
+    }
+
+    public Boolean getWasRead() {
+        return wasRead;
+    }
+
+    public void setWasRead(Boolean wasRead) {
+        this.wasRead = wasRead;
+    }
 }
