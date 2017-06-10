2b9b5e677063ae4fe7a97b5818cbf676369354fc
igor-kopec <igokop99@gmail.com>
2017-06-10 18:19:42 +0200

    added models + small fixes

---
+++ src/main/web/app/events/model/EventType2.ts
@@ -0,0 +1,36 @@
+export class EventType2{
+
+    title: string;
+    type: number;
+    time: any;
+    photo: string;
+    people_quantity: number;
+    dish_name: string;
+    dish_kind: number;
+    description: string;
+    date: any;
+    address: string;
+    additional_info: string;
+    products_list: string;
+    shopping_list: string;
+    quantity_of_products: any;
+    ownerId: number;
+    
+    constructor() {
+        this.title = '';
+        this.type = 2;
+        this.time = '';
+        this.photo = '';
+        this.people_quantity = 0;
+        this.dish_name = '';
+        this.dish_kind = 0;
+        this.description = '';
+        this.date = '';
+        this.address = '';
+        this.additional_info = '';
+        this.ownerId = -1;
+        this.products_list = '';
+        this.shopping_list = '';
+        this.quantity_of_products = 0;
+    }
+}
