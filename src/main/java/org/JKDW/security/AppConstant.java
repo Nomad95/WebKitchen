8c5933079a288044e3d8a1e47b07cc2ab654b840
igor-kopec <igokop99@gmail.com>
2017-06-14 20:09:30 +0200

    First commit

--- src/main/java/org/JKDW/security/AppConstant.java
+++
@@ -1,9 +0,0 @@
-package org.JKDW.security;
-
-
-public class AppConstant {
-
-    /** Przechowywana jest nazwa  nagłówka **/
-
-    public static final String tokenHeader = "X-Auth-Token";
-}
--- src/main/java/org/JKDW/user/model/DishKindEnum.java
+++
@@ -1,8 +0,0 @@
-package org.JKDW.user.model;
-
-/**
- * Enum containing type of dishes for database purposes
- */
-public enum DishKindEnum {
-    Śniadanie, Obiad, Kolacja, Deser
-}
