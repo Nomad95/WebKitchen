b134d4c23229e21db702bfac8a86afd82bc51403
intelcan <intelcan.idea@gmail.com>
2017-06-27 09:06:07 +0200

    stomp test

---
+++ src/main/java/org/JKDW/websocket/MarcoController.java
@@ -0,0 +1,7 @@
+package org.JKDW.websocket;
+
+/**
+ * Created by intelcan on 27.06.17.
+ */
+public class MarcoController {
+}
---
+++ src/main/java/org/JKDW/websocket/model/Shout.java
@@ -0,0 +1,7 @@
+package org.JKDW.websocket.model;
+
+/**
+ * Created by intelcan on 27.06.17.
+ */
+public class Shout {
+}
