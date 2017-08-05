32f01fc6f5fcccc884bcf8cde3a8ddb9df665fb6
intelcan <intelcan.idea@gmail.com>
2017-08-05 20:28:41 +0200

    SupportComponent v1

--- src/main/java/org/JKDW/mail/controller/MailController.java
+++ src/main/java/org/JKDW/mail/controller/MailController.java
@@ -49,7 +49,7 @@ public class MailController {
             consumes = MediaType.APPLICATION_JSON_VALUE)
     private ResponseEntity<Boolean> sendEmailFromUser(@RequestBody MailDTO mailDTO,
                                                       HttpServletRequest request){
-        mailDTO.setEmail("borucharek666@gmail.com");
+        mailDTO.setEmail("kuchniatest@gmail.com");
         mailService.sendSimpleMessageFromUser(UserAccountService.getMyUsernameFromToken(request, tokenUtils ),mailDTO.getEmail(),mailDTO.getTitle(),mailDTO.getContent());
         return new ResponseEntity<>(true, HttpStatus.OK);
     }
